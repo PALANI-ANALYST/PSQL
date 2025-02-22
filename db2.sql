a2a5761bd08c8a215c2e2b23847a328bac4280be
PALANI-ANALYST <palanianalyst@gmail.com>
2025-02-22 14:14:41 +0530

    Updated repo connection

--- db2.sql
+++ db2.sql
@@ -1,27 +1,2 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 17.4
--- Dumped by pg_dump version 17.4
-
--- Started on 2025-02-22 13:52:30
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
--- Completed on 2025-02-22 13:52:31
-
---
--- PostgreSQL database dump complete
---
 
