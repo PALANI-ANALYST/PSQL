247d5ce325be9780fc5bbcb76767907a67b6dbe2
ThunderFury-csk <3sekarcsk@gmail.com>
2025-03-11 13:29:11 +0530

    dhaush

--- Practice.sql
+++ Practice.sql
@@ -4,7 +4,7 @@ WHERE datname = 'db2';
 SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'db2';
 
 
-
+dskndskanfdsa
 
 -- Create the student_batch table
 CREATE TABLE student_batch (
